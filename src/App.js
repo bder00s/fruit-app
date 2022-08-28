bf17c69b4e28ca86b4b108597f303f6d22b2d148
bder00s <bonne-deroos@hotmail.com>
2022-08-28 09:38:43 +0200

    opzet project

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import './App.css';
 function App() {
   return (
     <main>
-      Begin hier met de tutorial!
+        <article>
+            <h2>Citroen</h2>
+            <p>Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten. Van citroen kun je het vruchtvlees, het sap en de schil gebruiken. Het sappige, lichtgele zure vruchtvlees versterkt de smaak van ander voedsel.</p>
+
+        </article>
+
     </main>
   );
 }
